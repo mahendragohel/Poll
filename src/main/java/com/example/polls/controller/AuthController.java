0c4df3d3213a31a975187890bafbb930c333b312
unknown <mahendra0139@gmail.com>
2018-09-16 01:05:45 +0530

    added react build

--- src/main/java/com/example/polls/controller/AuthController.java
+++ src/main/java/com/example/polls/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.example.polls.controller;
 
 import com.example.polls.exception.AppException;
+import com.example.polls.exception.ResourceNotFoundException;
 import com.example.polls.model.Role;
 import com.example.polls.model.RoleName;
 import com.example.polls.model.User;
@@ -61,11 +62,12 @@ public class AuthController {
                         loginRequest.getPassword()
                 )
         );
-
+        User user = userRepository.findByUsername(loginRequest.getUsernameOrEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "username", loginRequest.getUsernameOrEmail()));
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         String jwt = tokenProvider.generateToken(authentication);
-        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+        return ResponseEntity.ok(new JwtAuthenticationResponse(jwt,user.getRoles()));
     }
 
     @PostMapping("/signup")
--- src/main/java/com/example/polls/payload/JwtAuthenticationResponse.java
+++ src/main/java/com/example/polls/payload/JwtAuthenticationResponse.java
@@ -1,17 +1,31 @@
 package com.example.polls.payload;
 
+import java.util.Set;
+
+import com.example.polls.model.Role;
+
 /**
  * Created by rajeevkumarsingh on 19/08/17.
  */
 public class JwtAuthenticationResponse {
     private String accessToken;
     private String tokenType = "Bearer";
-
-    public JwtAuthenticationResponse(String accessToken) {
+    private Set<Role> userRoles;
+    
+    public JwtAuthenticationResponse(String accessToken,Set<Role> roles) {
         this.accessToken = accessToken;
+        this.userRoles = roles;
     }
 
-    public String getAccessToken() {
+    public Set<Role> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<Role> userRoles) {
+		this.userRoles = userRoles;
+	}
+
+	public String getAccessToken() {
         return accessToken;
     }
 
